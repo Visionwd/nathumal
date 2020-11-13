b211969be27166cfa0fe5dde699e697db588513a
netpixeltech <netpixeltech@gmail.com>
2020-11-13 13:01:40 +0530

    removed jquery

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,25 +15,6 @@ module.exports = {
         accessToken: `TvZqr9I0vb1DNOo84R8Jrh44lc981-vgJtBuIeQghqc`,
       },
     },
-    {
-      resolve: "gatsby-source-apiserver",
-      options: {
-        // Type prefix of entities from server
-        typePrefix: "internal__",
   
-        // The url, this should be the endpoint you are attempting to pull data from
-        url: `http://www.heydemo.ml/test.php`,
-  
-        method: "post",
-  
-        headers: {
-          "Content-Type": "application/json"
-        },
-  
-        // Request body
-        data: {
-          myvalue:"hello"
-        },
-      },}  
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,11 @@
-exports.onCreateWebpackConfig = ({
-    actions,
-  }) => {
-    const { setWebpackConfig } = actions;
-    setWebpackConfig({
-      externals: {
-        jquery: 'jQuery', // important: 'Q' capitalized
-        'window.jQuery': 'jquery'
-      }
-    })
-  }
+// exports.onCreateWebpackConfig = ({
+//     actions,
+//   }) => {
+//     const { setWebpackConfig } = actions;
+//     setWebpackConfig({
+//       externals: {
+//         jquery: 'jQuery', // important: 'Q' capitalized
+//         'window.jQuery': 'jquery'
+//       }
+//     })
+//   }
